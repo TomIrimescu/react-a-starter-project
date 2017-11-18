42de6d2fb1d5da8072187f17cd8fe00a65b155f1
TomIrimescu <tom@irimescu.com>
2017-11-18 13:32:30 -0500

    Initial commit w/material-ui, material-ui-icons and radium dependencies added along with FormDialog and SimpleSnackBar components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import './App.css';
+import Button from 'material-ui/Button';
+import FormDialog from './components/FormDialog';
+import SimpleSnackBar from './components/SimpleSnackBar';
+
+class App extends Component {
+  
+    render() {
+    const style = {
+      textAlign: 'center'
+    };
+    
+    return (
+      <div style={style}>
+        <h3>Hello, I am a react starter project</h3>
+        <p>This starter project includes Material-UI [1.0.0-beta.21].</p>
+        <p> The following is a Material-UI button, Form Dialog and SnackBar.</p>
+        <Button raised color="accent">Hello World</Button>
+        <p><FormDialog /></p>
+        <p><SimpleSnackBar /></p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
