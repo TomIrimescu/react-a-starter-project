55d5b0f6b2bc2880f668364e1cd21dbacf619aec
TomIrimescu <tom@irimescu.com>
2017-11-18 14:15:26 -0500

    react-scripts ejected, configured webpack(Dev/Prod) css-loader to use CSS Modules and created sample module class 'blue'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import AppStyle from './App.css';
 import Button from 'material-ui/Button';
 import FormDialog from './components/FormDialog';
 import SimpleSnackBar from './components/SimpleSnackBar';
@@ -13,7 +13,7 @@ class App extends Component {
     
     return (
       <div style={style}>
-        <h3>Hello, I am a react starter project</h3>
+        <h3 className={AppStyle.blue}>Hello, I am a react starter project</h3>
         <p>This starter project includes Material-UI [1.0.0-beta.21].</p>
         <p> The following is a Material-UI button, Form Dialog and SnackBar.</p>
         <Button raised color="accent">Hello World</Button>
